efd9505bbccdf043be98a157fb569e46c792ee80
Virendra Warshetti <55187761+iamtechbegger@users.noreply.github.com>
2020-08-06 15:32:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to Integration!!');
 });
 
+app.get('/api/test', (req, res) => {
+    res.send('Welcome to Integration api test me aaya gaya!!');
+});
+
 const port = process.env.PORT || 8999;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
